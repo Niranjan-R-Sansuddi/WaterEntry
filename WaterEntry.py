bcda8f8f592583b0992328e893fd84bb51746ce3
Niranjan-R-Sansuddi <111779160+Niranjan-R-Sansuddi@users.noreply.github.com>
2023-06-23 11:37:21 +0530

    Update WaterEntry.py

--- WaterEntry.py
+++ WaterEntry.py
@@ -41,6 +41,9 @@ date_str = st.date_input('Date', value=current_date).strftime('%Y-%m-%d')
 st.subheader('Water Input')
 idc_water_1 = st.text_input('IDC Water-1')
 idc_water_2 = st.text_input('IDC Water-2')
+water_tanker=st.text_input('Water Tanker ')
+rain_water_r_d=st.text_input('Rain Water R and D')
+rain_water_main_building=st.text_input('Rain Water Main Building')
 
 # Industrial Usage section
 st.subheader('Industrial Usage')
@@ -52,6 +55,9 @@ fl_ca_lab = st.text_input('FL CA Lab')
 tl_ca_lab = st.text_input('TL CA Lab')
 solar_panel_cleaning = st.text_input('Solar Panel Cleaning')
 injection_moulding_press_cooling_tower = st.text_input('Injection Moulding / Press Cooling Tower')
+filteration_water_cycle=st.text_input('Filteration Water Fresh')
+filteration_water_recycle=st.text_input('Filteration Recycle')
+ro_system=st.text_input('RO System [R/D]')
 
 # Non Industrial Usage section
 st.subheader('Non Industrial Usage')
@@ -61,26 +67,35 @@ mkt_office_toilet = st.text_input('Mkt/Office Toilet')
 operator_toilet_1 = st.text_input('Operator Toilet 1')
 operator_toilet_2 = st.text_input('Operator Toilet 2')
 canteen_service_center = st.text_input('Canteen/Service Center')
+paint_shop_toilet=st.text_input('Paint Shop Toilet')
+canteen_kitchen=st.text_input('Canteen Kitchen')
+main_gate_security=st.text_input('Main Gate Security')
 
 # Submit button
 if st.button('Submit'):
     if not all([date_str, idc_water_1, idc_water_2, paint_shop, r_and_d, tl_press_cooling_tower,
             fl_tl_line, fl_ca_lab, tl_ca_lab, solar_panel_cleaning,
             injection_moulding_press_cooling_tower, r_d_toilet, r_d_water_intake,
-            mkt_office_toilet, operator_toilet_1, operator_toilet_2, canteen_service_center]):
+            mkt_office_toilet, operator_toilet_1, operator_toilet_2, canteen_service_center,water_tanker,
+            rain_water_main_building,rain_water_r_d,ro_system,paint_shop_toilet,canteen_kitchen,
+            main_gate_security,filteration_water_cycle,filteration_water_recycle]):
         st.error('Please fill in all fields.')
     else:   
         if not sheet.get('A1'):
             column_names=['Date', 'idc_water_1', 'idc_water_2', 'paint_shop', 'r_and_d', 'tl_press_cooling_tower',
             'fl_tl_line', 'fl_ca_lab', 'tl_ca_lab', 'solar_panel_cleaning',
             'injection_moulding_press_cooling_tower', 'r_d_toilet', 'r_d_water_intake',
-            'mkt_office_toilet', 'operator_toilet_1', 'operator_toilet_2', 'canteen_service_center']
+            'mkt_office_toilet', 'operator_toilet_1', 'operator_toilet_2', 'canteen_service_center','water_tanker',
+            'rain_water_main_building','rain_water_r_d','ro_system','paint_shop_toilet','canteen_kitchen',
+            'main_gate_security','filteration_water_cycle','filteration_water_recycle']
             sheet.insert_row(column_names, index=1)
         # Store the form data in Google Sheets
         row = [date_str, idc_water_1, idc_water_2, paint_shop, r_and_d, tl_press_cooling_tower,
             fl_tl_line, fl_ca_lab, tl_ca_lab, solar_panel_cleaning,
             injection_moulding_press_cooling_tower, r_d_toilet, r_d_water_intake,
-            mkt_office_toilet, operator_toilet_1, operator_toilet_2, canteen_service_center]
+            mkt_office_toilet, operator_toilet_1, operator_toilet_2, canteen_service_center,water_tanker,
+            rain_water_main_building,rain_water_r_d,ro_system,paint_shop_toilet,canteen_kitchen,
+            main_gate_security,filteration_water_cycle,filteration_water_recycle]
         sheet.append_row(row)
         st.success('Data Uploaded successfully!')
 
